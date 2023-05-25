bcf1cace3fcbbd48b95050c247a6281da5eb9d74
JEFFERSON LINS DE OLIVEIRA <jefferson.loliveira9@senacsp.edu.br>
2023-05-25 17:00:07 -0300

    Atualizando Sistema OS

---
+++ PrimeiroSpring1/src/test/java/com/projeto/primeirospring/PrimeiroSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.primeirospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
