b176928977fe156f9a2979b4a636de093ce84c96
JEFFERSON LINS DE OLIVEIRA <jefferson.loliveira9@senacsp.edu.br>
2023-06-06 17:02:12 -0300

    Atualizando Pastas

---
+++ Atividade 06-06-23/AcessoBanco-1/src/main/java/com/projeto/acesso/AcessoBanco1Application.java
@@ -0,0 +1,13 @@
+package com.projeto.acesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessoBanco1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessoBanco1Application.class, args);
+	}
+
+}
---
+++ Atividade 06-06-23/AcessoBanco-1/src/test/java/com/projeto/primeirospring/AcessoBanco1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.primeirospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcessoBanco1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
